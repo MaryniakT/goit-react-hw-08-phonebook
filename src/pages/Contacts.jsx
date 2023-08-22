1a1ee3c7cd07deeb4285b940e062d6dd26580ef8
MaryniakT <114291060+MaryniakT@users.noreply.github.com>
2023-08-22 14:21:20 +0300

    contacts

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,21 +1,23 @@
-import { ContactForm } from "components/ContactForm/ContactForm";
-import { Filter } from "components/Filter/Filter";
-import { ContactList } from "components/ContactList/ContactList";
-import { useDispatch, userDispatch, useSelector } from 'react-redux';
-import { useEffect } from "react";
-import { selectContact, selectLoading } from "redux/selectors";
-import { fetchContacts } from "redux/contacts/operations";
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { Filter } from 'components/Filter/Filter';
+import { ContactList } from 'components/ContactList/ContactList';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { selectContact, selectLoading } from 'redux/selectors';
+import { fetchContacts } from 'redux/contacts/operations';
 import { Container } from 'components/ContactForm/ContactForm.styled';
 
 const Contacts = () => {
-    const dispatch = useDispatch();
-    const isLoading = useSelector(selectLoading);
-    const contacts = useSelector(selectContact);
-    useEffect(() => {
-        dispatch(fetchContacts());
-    }, [dispatch]);
-    return (
-         <Container>
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectLoading);
+  const contacts = useSelector(selectContact);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <Container>
       <div>
         <h1>Phonebook</h1>
         <ContactForm />
